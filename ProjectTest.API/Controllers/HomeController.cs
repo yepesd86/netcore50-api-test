29641baa13e5a09897bd7a26b5cb45c5b37f2159
Stywar <franco111988@gmail.com>
2021-02-20 22:56:24 -0500

    Se agrego apellidos

--- ProjectTest.API/Controllers/HomeController.cs
+++ ProjectTest.API/Controllers/HomeController.cs
@@ -18,7 +18,7 @@ namespace ProjectTest.API.Controllers
         public string Get()
         {
             _logger.LogInformation("Initial Home Get");
-            return "Net Core 5.0 Running ... v16012021-Stywar - Jenkinsfile Tag";
+            return "Net Core 5.0 Running ... v16012021-Stywar-Vargas - Jenkinsfile Tag";
         }
     }
 }
