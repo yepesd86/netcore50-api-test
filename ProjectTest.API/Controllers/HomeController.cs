4428169d462310e5096bd55bb4d4201cb5e6ae63
Stywar <franco111988@gmail.com>
2021-02-21 22:19:56 -0500

    se agrego Devops

--- ProjectTest.API/Controllers/HomeController.cs
+++ ProjectTest.API/Controllers/HomeController.cs
@@ -18,7 +18,7 @@ namespace ProjectTest.API.Controllers
         public string Get()
         {
             _logger.LogInformation("Initial Home Get");
-            return "Net Core 5.0 Running ... v16012021-Stywar-Vargas-214-PIPELINE2 - Jenkinsfile Tag";
+            return "Net Core 5.0 Running ... v21022021-Stywar AZURE DEVOPS";
         }
     }
 }
