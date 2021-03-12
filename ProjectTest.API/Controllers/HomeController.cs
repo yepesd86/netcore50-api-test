08ed0a2ef3db8b98371d6f0608c545023a971fa5
Stywar <franco111988@gmail.com>
2021-03-11 21:38:22 -0500

    se agrego fecha

--- ProjectTest.API/Controllers/HomeController.cs
+++ ProjectTest.API/Controllers/HomeController.cs
@@ -18,7 +18,7 @@ namespace ProjectTest.API.Controllers
         public string Get()
         {
             _logger.LogInformation("Initial Home Get");
-            return "Net Core 5.0 Running ... v21022021-2";
+            return "Net Core 5.0 Running ... 11032021 AFORO255";
         }
     }
 }
