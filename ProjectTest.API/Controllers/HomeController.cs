891fa7e11553659df4588975c59ba065bdb6b543
yepesd86 <diegoyepez@gmail.com>
2021-06-12 21:00:39 -0500

    Update HomeController.cs

--- ProjectTest.API/Controllers/HomeController.cs
+++ ProjectTest.API/Controllers/HomeController.cs
@@ -18,7 +18,7 @@ namespace ProjectTest.API.Controllers
         public string Get()
         {
             _logger.LogInformation("Initial Home Get");
-            return "Net Core 5.0 Running ... 12062021-v1.0.0 AzureDevops AFORO255";
+            return "Net Core 5.0 Running ... 12062021-v1.0.0 AzureDevops AFORO255 Juan Diego Yepes";
         }
     }
 }
